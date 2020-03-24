5550668668e0e91fe8ab2d915bf93bf614de6697
Paul Li <paul.chet.sebs.li@gmail.com>
2020-03-24 13:23:51 -0400

    3.1 and best square step 1

---
+++ 3_stack_queue/3.1_three_in_one.rb
@@ -0,0 +1,88 @@
+class ThreeInOne
+  def initialize(len)
+    @data = Array.new(len, nil)
+    @sizes = Array.new(3, 0)
+    @capacities = [len/3, len/3, len - 2*len/3]
+  end
+
+  def is_empty?(stack_num)
+    @sizes[stack_num-1].zero?
+  end
+  
+  def is_full?(stack_num)
+    @sizes[stack_num-1] >= @capacities[stack_num-1]
+  end
+
+  def top_index(stack_num)
+    @sizes[stack_num-1] + @data.size / 3 * (stack_num-1) - 1
+  end
+
+  def peek(stack_num)
+    puts "PEEKING STACK #{stack_num}"
+    raise "Stack #{stack_num} is empty!" if is_empty?(stack_num)
+    puts "STACK #{stack_num} top is #{@data[top_index(stack_num)]}"
+    @data[top_index(stack_num)]
+  end
+
+  def pop(stack_num)
+    puts "POPPING STACK #{stack_num}"
+    data_to_return = peek(stack_num)
+    @sizes[stack_num-1] -= 1
+    puts "POPPED #{data_to_return} from STACK #{stack_num}"
+    data_to_return
+  end
+
+  def add(item, stack_num)
+    puts "ADDING #{item} to STACK #{stack_num}"
+    raise "Stack #{stack_num} is full!" if is_full?(stack_num)
+    @data[top_index(stack_num)+1] = item
+    @sizes[stack_num-1] += 1
+    top_index(stack_num)
+  end
+end
+
+def test
+  puts "INIT with length 7"
+  t = ThreeInOne.new(7)
+
+  t.add(1,3)
+  t.add(2,2)
+
+  begin
+    t.peek(1)
+  rescue
+    puts "ERROR: Stack 1 is empty"
+  end
+  t.peek(3)
+  t.peek(2)
+
+  t.add(3,3)
+  t.add(7,3)
+  t.add(4,2)
+
+  begin
+    t.peek(3)
+  rescue
+    puts "ERROR: Stack 3 is full"
+  end
+
+  begin
+    t.peek(2)
+  rescue
+    puts "ERROR: Stack 2 is full"
+  end
+
+  begin
+    t.pop(1)
+  rescue
+    puts "ERROR: Stack 1 is empty"
+  end
+
+  t.pop(2)
+  t.pop(2)
+  begin
+    t.pop(2)
+  rescue
+    puts "ERROR: Stack 2 is empty"
+  end
+end
--- interview-log/best_square.rb
+++ interview-log/best_square.rb
@@ -4,4 +4,82 @@ Given a 2D matrix m and an integer k, complete the following tasks
 1. find the maximal sum of all possible k x k sub-matrices within m
 2. Compute and return the sum of all distinct elements within all k x k sub-matrices of the maximal sum 
 
+Assume m is a non-empty matrix, and k is valid with respect to m's size
+
 =end
+
+require 'pp'
+require 'set'
+
+class BestSquare
+  class Coordinate
+    attr_reader :x, :y
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+  end
+
+  def initialize(m, k)
+    @m = m
+    @k = k
+    @rows, @cols = m.size, m[0].size
+    precompute_first_k_entries
+    find_max_sum
+  end
+
+  def precompute_first_k_entries
+    @first_k_entries = []
+    0.upto(@rows-1) do |x|
+      @first_k_entries << (0..@k-1).inject(0) { |sum, y| sum + @m[x][y] }
+    end
+  end
+  
+  def find_max_sum
+    @max_sum, @coordinates = 0, []
+    0.upto(@cols-@k) do |y|
+      sum = 0
+      0.upto(@rows-@k) do |x|
+        if y.zero?
+          if x.zero?
+            sum = (x..x+@k-1).inject(0) { |result, i| result + @first_k_entries[i]}
+            @max_sum = sum
+          else
+            sum += (@first_k_entries[x+@k-1] - @first_k_entries[x-1])
+          end
+        else
+          if x.zero?
+            sum = (x..x+@k-1).inject(0) do |result, i|
+              @first_k_entries[i] += (@m[x+i][y+@k-1] - @m[x+i][y-1]) 
+              result + @first_k_entries[i]
+            end
+          else
+            @first_k_entries[x+@k-1] += (@m[x+@k-1][y+@k-1] - @m[x+@k-1][y-1]) 
+            sum += (@first_k_entries[x+@k-1] - @first_k_entries[x-1])
+          end
+        end
+
+        if sum >= @max_sum
+          if sum > @max_sum
+            @max_sum = sum
+            @coordinates = []
+          end
+          @coordinates.push(Coordinate.new(x,y))
+        end
+      end
+    end
+  end
+
+  def sum_distinct_element
+    set = Set.new
+  end
+end
+
+def test
+  m = [
+    [1,2,3,4],
+    [2,5,6,7],
+    [5,6,8,1],
+    [10,2,9,4],
+  ]
+  bs = BestSquare.new(m, 2)
+end
