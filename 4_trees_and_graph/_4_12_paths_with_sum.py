d6633b7d47bf9bd1c427cfbd08f8000b6e4b43cb
Paul Li <paul.chet.sebs.li@gmail.com>
2020-04-22 17:22:29 -0400

    better and faster 4.12

--- 4_trees_and_graph/_4_12_paths_with_sum.py
+++ 4_trees_and_graph/_4_12_paths_with_sum.py
@@ -4,30 +4,46 @@ sys.path.append("/Users/pauLi/Documents/Interviews/algo-practice/")
 from bi_node import BiNode
 
 def paths_with_sum(root, target):
-  result = [0]
-  compute_sum_map(root, target, result)
-  return result[0]
+  return compute_occurances(root, target, 0, {})
 
-def compute_sum_map(root, target, result):
+def compute_occurances(root, target, running_sum, occur_hash):
   if root is None:
-    return {}
-  left = compute_sum_map(root.left_node, target, result) 
-  right = compute_sum_map(root.right_node, target, result)
-  current = combine_and_increment(left, right, root.data)
-  result[0] += current.get(target, 0)
-  return current
+    return 0
+  running_sum += root.data
+  occurances = occur_hash.get(running_sum-target, 0)
+  occurances += 1 if running_sum == target else 0
+  
+  occur_hash[running_sum] = occur_hash.get(running_sum, 0) + 1
+  occurances += compute_occurances(root.left_node, target, running_sum, occur_hash)
+  occurances += compute_occurances(root.right_node, target, running_sum, occur_hash)
+  
+  occur_hash[running_sum] -= 1
+  if occur_hash[running_sum] == 0:
+    occur_hash.pop(running_sum)
+  
+  return occurances
 
-def combine_and_increment(left, right, additive):
-  current = {}
-  for key in left:
-    current[key + additive] = left[key] + right.get(key, 0)
+# SLOW!
+# def compute_sum_map(root, target, result):
+#   if root is None:
+#     return {}
+#   left = compute_sum_map(root.left_node, target, result) 
+#   right = compute_sum_map(root.right_node, target, result)
+#   current = combine_and_increment(left, right, root.data)
+#   result[0] += current.get(target, 0)
+#   return current
+
+# def combine_and_increment(left, right, additive):
+#   current = {}
+#   for key in left:
+#     current[key + additive] = left[key] + right.get(key, 0)
   
-  for key in right:
-    if key not in left:
-      current[key + additive] = right[key]
+#   for key in right:
+#     if key not in left:
+#       current[key + additive] = right[key]
   
-  current[additive] = current.get(additive, 0) + 1
-  return current
+#   current[additive] = current.get(additive, 0) + 1
+#   return current
 
 def test():
   root = None
