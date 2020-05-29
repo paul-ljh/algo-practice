e261b0697578b5ec5b1d645087dd81fe59c0035f
Paul Li <paul.chet.sebs.li@gmail.com>
2020-05-29 11:51:35 -0400

    longest increasing sequence

---
+++ classic_problems/longest_increasing_sequence.py
@@ -0,0 +1,47 @@
+from sys import maxsize
+
+def longest_increasing_sequence(seq):
+  length_list = [[]]
+  for num in seq:
+    best_index = find_best_lineup(length_list, num)
+    curr_index = best_index + 1
+    if curr_index > len(length_list)-1:
+      length_list.append([])
+    length_list[curr_index] = length_list[best_index] + [num]
+  return length_list[-1]
+
+def find_best_lineup(length_list, num):
+  left, right, best = 0, len(length_list)-1, 0
+  while right >= left:
+    mid = (left+right) // 2
+    pivot = length_list[mid][-1] if length_list[mid] else -maxsize-1
+    if pivot > num:
+      right = mid - 1
+    else:
+      left = mid + 1
+      best = mid
+  return best
+
+def test():
+  seq = []
+  print('PASS' if longest_increasing_sequence(seq) == [] else 'FAIL')
+  
+  seq = [1]
+  print('PASS' if longest_increasing_sequence(seq) == [1] else 'FAIL')
+  
+  seq = [1,2]
+  print('PASS' if longest_increasing_sequence(seq) == [1,2] else 'FAIL')
+  
+  seq = [1,0]
+  print('PASS' if longest_increasing_sequence(seq) == [0] else 'FAIL')
+  
+  seq = [1,4,3]
+  print('PASS' if longest_increasing_sequence(seq) == [1,3] else 'FAIL')
+  
+  seq = [1,3,2,0,9,6,10,12,4]
+  print('PASS' if longest_increasing_sequence(seq) == [1,2,6,10,12] else 'FAIL')
+
+if __name__ == '__main__':
+    test()
+    
+  
