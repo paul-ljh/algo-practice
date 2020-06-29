c09617acef11a053559cfe4dd16625c126e0cea9
Paul Li <paul.chet.sebs.li@gmail.com>
2020-06-29 10:49:22 -0400

    17.1 add without plus, twos complement, get sign

---
+++ 17_hard/_17_1_add_without_plus.py
@@ -0,0 +1,63 @@
+import sys
+sys.path.append("/Users/pauLi/Documents/Interviews/algo-practice/")
+
+from bit_ops import get_sign, twos_comp
+from sys import maxsize
+
+def add_without_plus(n1, n2):
+  if n1 == 0 or n2 == 0:
+    return n1 | n2
+  
+  n1_sign, n2_sign = get_sign(n1), get_sign(n2)
+  result = carry_on = 0
+  mask = 1
+
+  while mask >> 64 == 0:
+    n1_bit, n2_bit = n1 & mask, n2 & mask
+    result |= (n1_bit ^ n2_bit ^ (carry_on << 1))
+
+    if carry_on == 0:
+      carry_on = n1_bit & n2_bit
+    else:
+      carry_on = n1_bit | n2_bit
+    mask <<= 1
+  
+  # Integer Overflow
+  if n1_sign == n2_sign and get_sign(result) != n1_sign:
+      result = maxsize if n1_sign else -maxsize-1
+  return twos_comp(result)
+
+def test():
+  n1 = 0
+  n2 = 0
+  answer = 0
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+  n1 = 0
+  n2 = 0b101011
+  answer = n2
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+  n1 = 0b110011
+  n2 = 0b101011
+  answer = 0b1011110
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+  n1 = maxsize
+  n2 = 0b11
+  answer = maxsize
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+  n1 = -2
+  n2 = -4
+  answer = -6
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+  n1 = -maxsize
+  n2 = -4
+  answer = -maxsize-1
+  print('PASS' if add_without_plus(n1, n2) == answer else 'FAIL')
+  
+if __name__ == "__main__":
+  test()
+  
--- bit_ops.py
+++ bit_ops.py
@@ -11,3 +11,20 @@ def set_bit(num, i):
     return None
   mask = 1 << i
   return mask | num
+
+'''
+Int in python has 64 bits
+1 if non-negative, 0 otherwise
+'''
+def get_sign(n):
+  sign_mask = 1 << 63
+  return int(n & sign_mask == 0)
+
+'''compute the 2's complement of val'''
+def twos_comp(bin_seq):
+  mask = 1 << 63
+  result = 0
+  result += bin_seq & (mask - 1)
+  if get_sign(bin_seq) == 0:
+    result -= mask
+  return result
