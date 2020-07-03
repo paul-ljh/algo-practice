b254e8f72a3ab20eff5e3834841a06344861a3d8
Paul Li <paul.chet.sebs.li@gmail.com>
2020-07-03 11:52:25 -0400

    16.3 intersection get rid of comments

--- 16_moderate/_16_3_intersection.py
+++ 16_moderate/_16_3_intersection.py
@@ -1,36 +1,3 @@
-# def intersection(p1, p2, p3, p4):
-#   x_range = calculate_range(p1[0], p2[0], p3[0], p4[0])
-#   y_range = calculate_range(p1[1], p2[1], p3[1], p4[1])
-#   if x_range[0] > x_range[-1] or y_range[0] > y_range[-1]:
-#     return None
-#   a1, b1, c1 = calculate_line(p1, p2)
-#   a2, b2, c2 = calculate_line(p3, p4)
-#   if a1 == a2:
-#     for p in [p1, p2, p3, p4]:
-#       if p[0] == x_range[0]:
-#         return p
-#   else:
-#     denom = c2 * a1 - a2 * c1
-#     x = (b2 * c1 - b1 * c2) / denom
-#     y = (b2 * a1 - b1 * a2) / denom
-#     if x_range[0] <= x <= x_range[-1] and y_range[0] <= y <= y_range[-1]:
-#       return (x,y)
-#     else:
-#       return None        
-  
-# def calculate_line(p1, p2):
-#   (x1, y1), (x2, y2) = p1, p2
-#   if x1 == x2:
-#     return (1, -x1, 0)
-#   elif y1 == y2:
-#     return (0, y1, 1)
-#   else:
-#     return (
-#       (y1-y2)/(x1-x2),
-#       (x2*y1 - x1*y2)/(x2-x1),
-#       1
-#     )
-
 from math import isinf;
 
 def calculate_range(c1, c2, c3, c4):
